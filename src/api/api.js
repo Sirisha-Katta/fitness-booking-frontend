40e6e7181c91351630993f5f63da4eaf0996a626
K S Sirisha <kssirisha@Ks-MacBook-Air.local>
2025-06-22 14:25:10 +0530

    url change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://fitness-booking.onrender.com'; // Replace with your actual API base URL
-const API_BASE_URL = 'http://127.0.0.1:8000'; // Replace with your actual API base URL
+const API_BASE_URL = 'https://fitness-booking.onrender.com'; // Replace with your actual API base URL
+// const API_BASE_URL = 'http://127.0.0.1:8000'; // Replace with your actual API base URL
 
 export const signup = async (userData) => {
     try {
